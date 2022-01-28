63841229fecdd0981108f87800497120e38d7fa4
Iuliia El Sammak <juliag875@gmail.com>
2022-01-28 13:41:08 -0500

    commit tree 67cc692769a19cb7eb7b89ded8717a94c1b47da1

--- index.js
+++ index.js
@@ -10,3 +10,55 @@ const messageConfig = {
       "Loki"
   ]
 };
+
+// Arrow Function
+const printCard = function () {
+  console.log(this.frontContent);
+  console.log(this.insideContent);
+  // Wow! Elegant! And notice the arrow function's `this` is the same `this`
+  // that printCard has; specifically, the `thisArg` that was passed to it
+  this.signatories.forEach((signatory) =>
+    console.log(`${this.closing[signatory]}, ${signatory}`)
+  );
+};
+
+printCard.call(messageConfig);
+
+
+
+
+// Use a thisArg to avoid the lost context bug
+// const printCard = function () {
+//   console.log(this.frontContent);
+//   console.log(this.insideContent);
+
+//   const contextBoundForEachExpr = function (signatory) {
+//     const message = `${this.closing[signatory]}, ${signatory}`;
+//     console.log(message);
+//   }.bind(this);
+
+//   this.signatories.forEach(contextBoundForEachExpr);
+// };
+// printCard.call(messageConfig);
+
+// console.log("Debug Before forEach: " + this);
+  // this.signatories.forEach(function (signatory) {
+  //   const message = `${this.closing[signatory]}, ${signatory}`;
+  //   console.log(message);
+  // },this;
+// printCard.call(messageConfig);
+
+// Closure
+// const printCard = function () {
+//   console.log(this.frontContent);
+//   console.log(this.insideContent);
+
+//   const outerContext = this;
+
+//   this.signatories.forEach(function (signatory) {
+//     const message = `${outerContext.closing[signatory]}, ${signatory}`;
+//     console.log(message);
+//   });
+// };
+
+// printCard.call(messageConfig);
